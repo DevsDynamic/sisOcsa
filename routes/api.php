79d3753b1e2f9fb82f5c9b7aa44ffca87d0fdc17
Proyecto14 <proyecto14@ferreycorp.onmicrosoft.com>
2026-08-24 01:18:18 -0500

    Habilita prueba manual publica de Osinergmin

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/send-data-osinergmin', [TaskController::class, 'sendDataOsinergmin'])
+    ->middleware('throttle:1,1')
+    ->name('osinergmins.send-data');
